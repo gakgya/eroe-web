ce9d627f1a47d12ceeac45e94025a6401ea8b076
psb <psb0902@naver.com>
2022-09-16 12:32:45 +0900

    0916

--- src/eroe/Map.js
+++ src/eroe/Map.js
@@ -1,22 +1,30 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from 'react';
 
-const Map=()=>{
+const { kakao } = window;
 
-  useEffect(()=>{
-    var container = document.getElementById('map');
-    var options = {
-      center: new kakao.maps.LatLng(37.365264512305174, 127.10676860117488),
-      level: 3
-    };
-    var map = new kakao.maps.Map(container, options);
-    }, [])
-
+const Location = () => {
+    
+    useEffect(() => {
+        const container = document.getElementById('myMap');
+		const options = {
+			center: new kakao.maps.LatLng(33.450701, 126.570667),
+			level: 3
+		};
+        const map = new kakao.maps.Map(container, options);
+        var markerPosition  = new kakao.maps.LatLng(37.365264512305174, 127.10676860117488); 
+        var marker = new kakao.maps.Marker({
+          position: markerPosition
+      });
+      marker.setMap(map);
+      
+    }, []);
 
     return (
-        <div>
-        	<div id="map" style={{width:"500px", height:"400px"}}></div> 
-        </div>
-    )
+        <div id='myMap' style={{
+            width: '500px', 
+            height: '500px'
+        }}></div>
+    );
 }
 
-export default Map;
+export default Location; 
