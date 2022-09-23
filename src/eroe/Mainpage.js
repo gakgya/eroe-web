59763ea3efb39b85f1909d5a4019d0a00b1f8fff
kjw <cxz7852@naver.com>
2022-09-23 11:01:50 +0900

    지도 성공

--- src/eroe/Mainpage.js
+++ src/eroe/Mainpage.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "./css/Mainpage.css";
-import Map from "./Map";
+import Location from "./Location";
 
 function Mainpage() {
   return (
     <div style={{ width: "100%" }}>
       <div className="stand">
-        <Map/>
+        <div className="map"><Location/></div>
         <div className="human">human1</div>
       </div>
       <footer className="call">call</footer>
