898a28033c52ccfd2a23d57644e00ba2352c89b5
kjw <cxz7852@naver.com>
2022-09-23 12:06:40 +0900

    맵 완료 직전, 디비 연결 모듈 설치

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ function App() {
         <Sidemenu />
         <div></div>
         <Mainpage />
-        
       </div>
     </div>
   );
--- src/eroe/Location.js
+++ src/eroe/Location.js
@@ -1,14 +1,17 @@
-import React from "react";
+import React, {useState} from "react";
 import { Map, MapMarker } from "react-kakao-maps-sdk";
 
 function Location(){
+  const [loc_lat, setLoc_lat] = useState(33.5563);
+  const [loc_lng, setLoc_lng] = useState(126.79581);
+  const text = '현재위치'
   return (
     <Map
-      center={{ lat: 33.5563, lng: 126.79581 }}
+      center={{ lat: loc_lat, lng: loc_lng }}
       style={{ width: "100%", height: "100%" }}
     >
-      <MapMarker position={{ lat: 33.55635, lng: 126.795841 }}>
-        <div style={{color:"#000"}}>Hello World!</div>
+      <MapMarker position={{ lat: loc_lat, lng: loc_lng }}>
+        <div style={{color:"#000"}}>{text}</div>
       </MapMarker>
     </Map>
   )
