f4dec01ed33d42f4b58cdd1276bc770688b86aff
psb <psb0902@naver.com>
2022-09-27 16:09:40 +0900

    5

--- src/eroe/Mainpage.js
+++ src/eroe/Mainpage.js
@@ -4,9 +4,21 @@ import Location from "./Location";
 import User from "./User";
 import Call from "./Call";
 
-function Mainpage() {
+function Mainpage(props) {
+
+  const isLoginPage = props.isLoginPage
+    
+  const onLogout = () => {
+    	// sessionStorage 에 user_id 로 저장되어있는 아이템을 삭제한다.
+        sessionStorage.removeItem('user_id')
+        // App 으로 이동(새로고침)
+        document.location.href = '/'
+    }
   return (
+    
     <div style={{ width: "100%" }}>
+      <h2>메인페이지</h2>
+      <button type='button' onClick={onLogout}>Logout</button>
       <div className="stand">
         <div className="map"><Location/></div>
         <div className="human"><User/></div>
