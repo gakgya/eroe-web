49a28110373cbbdcad0be2e7ba4382714d18d9d0
kjw <cxz7852@naver.com>
2022-09-27 15:50:51 +0900

    0927

---
+++ src/eroe/Call.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Call() {
+    return (
+        <div>
+            <p>112</p>
+        </div>
+    )
+}
+
+export default Call;
--- src/eroe/Mainpage.js
+++ src/eroe/Mainpage.js
@@ -1,15 +1,17 @@
 import React from "react";
 import "./css/Mainpage.css";
 import Location from "./Location";
+import User from "./User";
+import Call from "./Call";
 
 function Mainpage() {
   return (
     <div style={{ width: "100%" }}>
       <div className="stand">
         <div className="map"><Location/></div>
-        <div className="human">human1</div>
+        <div className="human"><User/></div>
       </div>
-      <footer className="call">call</footer>
+      <footer className="call">긴급 전화 번호 <Call/></footer>
     </div>
   );
 }
