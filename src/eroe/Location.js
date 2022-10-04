e1747550701b2adc48ad464cd164d0fd724e3cd9
gakgya <mirejiho@naver.com>
2022-10-04 14:29:39 +0900

    qw

--- src/eroe/Location.js
+++ src/eroe/Location.js
@@ -1,20 +1,20 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { Map, MapMarker } from "react-kakao-maps-sdk";
 
-function Location(){
+function Location() {
   const [loc_lat, setLoc_lat] = useState(33.5563);
   const [loc_lng, setLoc_lng] = useState(126.79581);
-  const text = '현재위치'
+  const text = "현재위치";
   return (
     <Map
       center={{ lat: loc_lat, lng: loc_lng }}
       style={{ width: "100%", height: "100%" }}
     >
       <MapMarker position={{ lat: loc_lat, lng: loc_lng }}>
-        <div style={{color:"#000"}}>{text}</div>
+        <div style={{ color: "#000" }}>{text}</div>
       </MapMarker>
     </Map>
-  )
+  );
 }
 
 export default Location;
