be4d1afd6f1848104b4fd7bce98de7eec3f886bc
jiuk <hss06201@naver.com>
2022-10-18 16:13:50 +0900

    지욱

---
+++ src/eroe/Report.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./css/Report.css";
+
+function Report () {
+    return(
+        <div>
+            <button onClick={{}} className="police_button">112에 신고하기</button>
+            <button onClick={{}} className="firefighter_button">119에 신고하기</button>
+        </div>
+    );
+}
+
+export default Report;
