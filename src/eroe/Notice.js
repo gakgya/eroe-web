f74f9a738dcfd29d1589beae7112d24267eadcca
jw <you@example.com>
2022-11-11 11:36:33 +0900

    221111

--- src/eroe/Notice.js
+++ src/eroe/Notice.js
@@ -3,7 +3,7 @@ import "./css/Notice.css";
 
 function Notice() {
   return (
-    <div className="standard">
+    <div id="notice_standard">
         <p className="notice_txt_odd">알림</p>
         <p className="notice_txt_even">&nbsp;'배남길'님이 외출하셨습니다.</p>
         <p className="notice_txt_odd">&nbsp;'배남길'님이 복귀하셨습니다.</p>
