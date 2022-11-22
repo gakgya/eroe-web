012b6fd0403faeffe25c67aa36ef3dd6b4cbff64
k <you@example.com>
2022-11-22 12:16:25 +0900

    221122

--- src/eroe/Locationinfo.js
+++ src/eroe/Locationinfo.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Location from "./Location";
 import "./css/Location_info.css";
+import user from "./img/user.png";
 
 function Location_info() {
   return (
@@ -8,6 +9,24 @@ function Location_info() {
       <div className="location_map">
         <Location />
       </div>
+      <div className = "standard location_user">
+          <div style={{backgroundColor: "white", borderRadius: 30}}>
+        <div className ='location_user_box'>     
+        <div className = 'user'>
+              <img
+                className="user_name"
+                src={user}
+                alt="user"
+                onClick={{}}     
+              />
+            </div>
+            <pre>
+              현재주소<br/>
+              신구대학교 남관
+            </pre>        
+          </div>        
+          </div>
+        </div>
     </div>
   );
 }
