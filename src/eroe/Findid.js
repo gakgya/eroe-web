bf5acd22f2275120c8c9ffb7128cf94b847abca7
psb <psb0902@naver.com>
2022-12-02 12:25:11 +0900

    cc

---
+++ src/eroe/Findid.js
@@ -0,0 +1,49 @@
+// import React from '/react'
+// import { useState } from "react";
+// import { Link, useNavigate } from "react-router-dom";
+// import "./css/Login.css";
+// function Findid() {
+
+    
+
+
+
+//     return(
+
+//         <div>
+
+//         <div className="Topbarstyle">
+//         <div className="Eroetext">eroe</div>
+//       </div>
+//       <div
+//         style={{
+//           display: "flex",
+//           width: "100%",
+//           height: "100%",
+//           flexDirection: "column",
+//         }}
+//       >
+//         <div className="input_standard">
+//           <label className="id_txt">아이디</label>
+//           <input
+//             className="id_box"
+//             type="id"
+//             value={id}
+//             onChange={onChangeId}
+//           />
+//           <label className="password_txt">비밀번호</label>
+//           <input
+//             className="password_box"
+//             type="password"
+//             value={password}
+//             onChange={onChangePassword}
+//           />
+            
+        
+//             </div>   
+//             </div>
+//             </div>
+//     )
+// }
+
+// export default Findid;
