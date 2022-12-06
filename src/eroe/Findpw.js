3ce8fdde5435ad3a4133be46d32bd66dd9606182
psb <psb0902@naver.com>
2022-12-06 13:43:52 +0900

    ㅊㅊ

--- src/eroe/Findpw.js
+++ src/eroe/Findpw.js
@@ -1,65 +1,63 @@
-import React from 'react'
+import React from "react";
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import "./css/Login.css";
 
 function Findpw() {
-    const [birth, setBirth] = useState("");
-    const [name, setName] = useState("");
-    const [birthCheck, setBirthcheck] = useState("True");
-    const [nameCheck, setnameCheck] = useState("True");
-  
+  const [birth, setBirth] = useState("");
+  const [id, setId] = useState("");
+  const [name, setName] = useState("");
+  const [birthCheck, setBirthcheck] = useState("True");
+  const [idcheck, setIdcheck] = useState("True");
+  const [nameCheck, setnameCheck] = useState("True");
 
-    function onChangeBirth(e) {
-        setBirth(e.target.value);
-        setBirthcheck("True");
-      }
+  function onChangeBirth(e) {
+    setBirth(e.target.value);
+    setBirthcheck("True");
+  }
 
-        function onChangeName(e) {
-        setName(e.target.value);
-        setnameCheck("True");
-
-      }
-
-    function idchecking() {
-        if (birth === "" ) {
-            alert("생년월일 입력해주십시오."); 
-            return;
-         }
-        if (name === "") {
-            alert('이름을 입력해주십시오.')
-            return;
-        }
-        else {
-        const birthpost = { check_birth:birth };
-        const namepost = { check_name: name };
-        fetch("http://gakgya.iptime.org:3001/callbody_find", {
-          method: "post",
-          headers: {
-            "content-type": "application/json",
-          },
-          body: JSON.stringify(birthpost),
-          body: JSON.stringify(namepost),
-
-        })
-          .then((res) => res.json())
-          .then((json) => {
-            setBirthcheck(json.ck);
-            setnameCheck(json.ck);
-            if (json.ck == "True") {
-              alert("id 는", json.ck);
-              setBirth("");
-              setName("");
-            } else if (json.ck == "False") {
-              alert("정보가 일치하지 않습니다.");
-            }
-          });
-      }
+  function onChangeName(e) {
+    setName(e.target.value);
+    setnameCheck("True");
+  }
+  function idchecking() {
+    if (birth === "") {
+      alert("ID를 입력해주십시오.");
+      return;
     }
-    return(
+    if (name === "") {
+      alert("이름을 입력해주십시오.");
+      return;
+    } else {
+      const post = {
+        check_birth: birth,
+        check_name: name,
+      };
 
-        <div>
-             <div className="Topbarstyle">
+      fetch("http://gakgya.iptime.org:3001/callbody_find", {
+        method: "post",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify(post),
+      })
+        .then((res) => res.json())
+        .then((json) => {
+          setIdcheck(json.ck1);
+          console.log(json);
+          if (json.ck == "True") {
+            alert("id는 이거다" + json.ckh);
+            setBirth("");
+            setName("");
+          } else if (json.ck1 == "False") {
+            alert("정보가 일치하지 않습니다.");
+          }
+        });
+    }
+  }
+  return (
+    <div>
+      <div className="Topbarstyle">
         <div className="Eroetext">eroe</div>
       </div>
       <div
@@ -71,8 +69,6 @@ function Findpw() {
         }}
       ></div>
 
-     
-      
       <div
         style={{
           display: "flex",
@@ -82,7 +78,7 @@ function Findpw() {
         }}
       >
         <div className="input_standard">
-          <label className="id_txt">생년월일</label>
+          <label className="id_txt">ID</label>
           <input
             className="id_box"
             type="id"
@@ -90,18 +86,14 @@ function Findpw() {
             onChange={onChangeBirth}
           />
           <label className="password_txt">이름</label>
-          <input
-            className="password_box"
-            type="name"        
-            onChange={onChangeName}
-          />
-          <button onClick = {idchecking}>ID확인하기</button>
-            
-        
-            </div>   
-            </div>
-            </div>
-    )
-    }
+          <input className="password_box" type="name" onChange={onChangeName} />
+          <div>
+          <button className = 'signup_button2'style = {{marginTop : 15}}onClick={idchecking}>비밀번호 찾기</button>
+        </div>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default Findpw;
